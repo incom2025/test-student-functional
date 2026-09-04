f5e0bb1722c540696f70459484ce1fcabaa8fe4d
incom2025 <incomlviv@gmail.com>
2026-09-04 22:12:23 +0300

    Add main function and addition logic in main.py

---
+++ Lab_01/main.py
@@ -0,0 +1,13 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def main() -> None:
+    x = 10
+    y = 20
+    result = add(x, y)
+    print(f"Result: {result}")
+
+
+if __name__ == "__main__":
+    main()
